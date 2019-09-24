e7398f24eb1eb7b2f211ac007b6e668c063c557c
Adam <i@adamurban.de>
2019-09-24 20:27:40 +0200

    added risk prevention check as function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,19 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+
+exports.riskPreventionCheck = functions.https.onRequest((request, response) => {
+    const name = request.body.name;
+    const id = request.body.id;
+
+    let result;
+
+    if (name === 'Donald Duck') {
+        result = {id, score: 0};
+    } else {
+        result = {id, score: 100};
+    }
+
+    response.send(result);
+});
